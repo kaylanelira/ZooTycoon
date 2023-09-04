416472df182980d003a6cd874f2ff3f1bdfa7c86
Kaylane Lira <kgl@cin.ufpe.br>
2023-09-04 14:52:33 -0300

    sql:add criacao de algumas tabelas

---
+++ Projeto Físico/consultas.sql
@@ -0,0 +1,12 @@
+Seu projeto deve ter todos os tipos de consultas abaixo
+-Group by/Having
+-Junção interna
+-Junção externa
+-Semi junção
+-Anti-junção
+-Subconsulta do tipo escalar
+-Subconsulta do tipo linha
+-Subconsulta do tipo tabela
+-Operação de conjunto
+
+Atenção: Cada aluno deve fazer ao menos 01 dessas consultas mais 01 procedimento com SQL embutida e parâmetro, função com SQL embutida e parâmetro ou gatilho. 
